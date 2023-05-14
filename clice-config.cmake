449bac245ce13de1241217d7fef72968e3d7e74b
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-15 00:02:17 +0200

    feat: add installation files

--- clice-config.cmake
+++ clice-config.cmake
@@ -5,4 +5,13 @@ endif()
 
 add_library(clice INTERFACE)
 add_library(clice::clice ALIAS clice)
-target_include_directories(clice INTERFACE ${CMAKE_CURRENT_LIST_DIR}/src)
+target_sources(clice INTERFACE FILE_SET set0 TYPE HEADERS BASE_DIRS src)
+target_include_directories(clice
+    INTERFACE
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/src>
+    $<INSTALL_INTERFACE:include>
+)
+include(GNUInstallDirs)
+install (TARGETS clice)
+install (DIRECTORY src/clice TYPE INCLUDE FILES_MATCHING PATTERN "*.h")
+install (FILES clice-config.cmake DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/)
