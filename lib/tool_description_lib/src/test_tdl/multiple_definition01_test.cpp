76d808122c0f71dc46746af0a5a31535d1594b4e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-10-05 21:33:11 +0200

    git subrepo clone (merge) https://github.com/deNBI-cibi/tool_description_lib.git lib/tool_description_lib
    subrepo:
      subdir:   "lib/tool_description_lib"
      merged:   "c4ab113"
    upstream:
      origin:   "https://github.com/deNBI-cibi/tool_description_lib.git"
      branch:   "main"
      commit:   "c4ab113"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ lib/tool_description_lib/src/test_tdl/multiple_definition01_test.cpp
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2023, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file.
+// -----------------------------------------------------------------------------------------------------
+
+#include <tdl/ToolInfo.h>
+#include <tdl/convertToCTD.h>
+
+
+/* This file, in combination with multiple_definition02_test.cpp are testing for multiple definition errors.
+ * Since this is a header only library, many functions are marked with `inline`. To avoid missing an `inline`
+ * keyword, we have two translation units with the same symbols. Linking these together will cause an multiple
+ * definition violation when the keyword `inline` is missing.
+ */
