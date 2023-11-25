f7aebc2ecacbcff93d8959a218a5b7caf3646d50
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-11-25 11:03:56 +0100

    patch: having a CMakeLists.txt for each target

--- clice-config.cmake
+++ clice-config.cmake
@@ -5,25 +5,6 @@ if (TARGET clice::clice)
     return()
 endif()
 
-add_library(clice INTERFACE)
-add_library(clice::clice ALIAS clice)
+find_package (fmt REQUIRED)
 
-option (CLICE_USE_TDL "Enables tool_description_lib(TDL) to be supported by CLICE (enables CWL features)" OFF)
-if (${ROOT_PROJECT})
-    set(CLICE_USE_TDL ON)
-endif()
-
-if (CLICE_USE_TDL)
-    find_package(tdl REQUIRED PATHS lib/tool_description_lib)
-    target_link_libraries(clice INTERFACE tdl::tdl)
-    target_compile_definitions(clice INTERFACE CLICE_USE_TDL)
-endif ()
-target_include_directories(clice
-    INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/src>
-    $<INSTALL_INTERFACE:include>
-)
-include(GNUInstallDirs)
-install (TARGETS clice)
-install (DIRECTORY src/clice TYPE INCLUDE FILES_MATCHING PATTERN "*.h")
-install (FILES clice-config.cmake DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src/clice ${CMAKE_CURRENT_BINARY_DIR}/clice)
