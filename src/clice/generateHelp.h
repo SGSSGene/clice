a9acfd5acf50c210a632af87a52de881594b3949
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-11-25 10:20:30 +0100

    fix: replace nullptr_t with std::nullptrvt

--- src/clice/generateHelp.h
+++ src/clice/generateHelp.h
@@ -17,7 +17,7 @@ inline auto typeToString(ArgumentBase const& arg) -> std::string {
             return t.substr(7);
         }
     }
-    if (arg.type_index == std::type_index(typeid(nullptr_t))) {
+    if (arg.type_index == std::type_index(typeid(std::nullptr_t))) {
         //!Nothing to do, this is a flag and doesn't take any parameters
         return "";
     } else if (arg.mapping) {
