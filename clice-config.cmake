c8513ddce58e154d4008a062da06e49c7580570c
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-10-17 22:18:34 +0200

    feat: use cpm.dependencies

--- clice-config.cmake
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Gottlieb+Freitag <info@gottliebtfreitag.de>
-# SPDX-License-Identifier: CC0-1.0
-cmake_minimum_required (VERSION 3.12)
-if (TARGET clice::clice)
-    return()
-endif()
-
-CPMaddPackage("gh:fmtlib/fmt#10.2.1")
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src/clice ${CMAKE_CURRENT_BINARY_DIR}/clice)
